e9c59dafb6e55210c5d4ac52e7c6756787c872fe
taha (gasgolfer.eth) <dhaileytaha@gmail.com>
2023-07-11 13:11:08 +0800

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="bg-black flex justify-center p-2 w-[100%] relative x-20 ">
       <div className="text-center text-white m-3  ">
         <h3 className="border-[1px]-[#C32F27] font-bold text-4xl my-4">
-          Content
+          Contact
         </h3>
         <p className="flex m-2 text-xl my-2">
           {" "}
